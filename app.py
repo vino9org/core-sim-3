a3a59d69ea72f6e46af77aadb98bdfe04ead1bf0
Li Lin <guru.lin@gmail.com>
2024-06-27 23:52:57 +0800

    update startup script

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ import logging.config
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
 from flask_orjson import OrjsonProvider
 from quart import Quart
 from tortoise.contrib.quart import register_tortoise
 
 from casa.api import blueprint as casa_api
 from casa.models import AccountModel  # noqa
+
+load_dotenv()
 
 with open(Path(__file__).parent / "logger_config.json", "r") as f:
     logging.config.dictConfig(json.load(f))
@@ -40,4 +43,4 @@ register_tortoise(
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8000)
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 cd "$(dirname "$0")"
 
 if [ -f ".env" ]; then
     source .env
+    env
 fi
 
-/bin/sleep 100000
+if [ "$DATABASE_URL" = "" ]; then
+    echo DATABASE_URL not set, aborting.
+    exit 1
+fi
+
+# migration etc.
+
+WRAPPER=
+
+if [ -f "newrelic.ini" ]; then
+    export NEW_RELIC_CONFIG_FILE=$(pwd)/newrelic.ini
+    WRAPPER="newrelic-admin run-program "
+fi
+
+$WRAPPER hypercorn app:app -b 0.0.0.0:8000 -w ${WORKERS:-1} 
