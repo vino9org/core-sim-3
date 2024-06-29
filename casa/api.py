43928f835e11f9c441d1b08802cd22e185a84159
Li Lin <guru.lin@gmail.com>
2024-06-30 01:13:40 +0800

    added handling of unexpected exception

--- casa/api.py
+++ casa/api.py
@@ -15,11 +15,15 @@ blueprint = Blueprint("casa", __name__)
 async def get_account_details(
     account_num: str,
 ):
-    account = await service.get_account_details(account_num)
-    if account:
-        return account.model_dump()
-    else:
-        return {"error": "Account not found or not active"}, 404
+    try:
+        account = await service.get_account_details(account_num)
+        if account:
+            return account.model_dump()
+        else:
+            return {"error": "Account not found or not active"}, 404
+    except Exception as e:
+        logger.warn(f"Unexpected expection: {str(e)}")
+        return Response({"error": str(e)}, status=500)
 
 
 @blueprint.route("/transfers", methods=["POST"])
@@ -28,7 +32,11 @@ async def transfer():
         transfer_req = s.TransferRequest.model_validate(await request.json)
         result, events = await service.transfer(transfer_req)
         current_app.add_background_task(service.publish_events, events)
+        logger.info(f"created transfer {result.trx_id}")
         return result.model_dump(), 201
     except (service.InvalidRequest, ValidationError) as e:
         logger.info(f"Invalid request: {str(e)}")
         return Response({"error": str(e)}, status=422)
+    except Exception as e:
+        logger.warn(f"Unexpected expection: {str(e)}")
+        return Response({"error": str(e)}, status=500)
